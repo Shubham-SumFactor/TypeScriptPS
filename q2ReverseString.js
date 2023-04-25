30af72e69c4401e3ed45b9a42b71f5ccbba301c3
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-04-25 16:56:26 +0530

    Q2 String Reverse done using ForLoop

--- q2ReverseString.js
+++ q2ReverseString.js
@@ -1,6 +1,15 @@
 var myString = "Anything";
 function reverseString(str) {
-    return str.split("").reverse().join(""); //splits into separate character
+    return str.split("").reverse().join(""); //splits into separate character , reverse n join
 }
 var output = reverseString(myString); //hold the result
 console.log(output);
+// Alternative method
+function stringReverse(string) {
+    var revString = "";
+    for (var i = string.length - 1; i >= 0; i--) {
+        revString = revString + string[i];
+    }
+    return revString;
+}
+console.log(stringReverse("Random"));
--- q2ReverseString.ts
+++ q2ReverseString.ts
@@ -8,4 +8,15 @@ const output = reverseString(myString); //hold the result
 
 console.log(output);
 
+// Alternative method
 
+function stringReverse(string:string): string{
+    let revString ="";
+    for (let i = string.length-1; i>=0; i--){
+        revString = revString + string[i];
+    }
+    return revString;
+}
+console.log(stringReverse("Random"));
+
+export{}
