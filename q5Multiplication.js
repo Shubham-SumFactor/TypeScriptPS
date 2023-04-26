0cdc2122d8d1ed748ec299e6d30eec9438e0fd0c
ShubhamSumFactor <shubham.sumfactor@gmail.com>
2023-04-26 09:52:58 +0530

    Q5 Multiplication W/o * operator

--- q5Multiplication.js
+++ q5Multiplication.js
@@ -1,6 +1,8 @@
 function multiplication(num1, num2) {
+    var maxNum = num1 > num2 ? num1 : num2;
+    var newMax = maxNum != num1 ? num1 : num2;
     var result = 0;
-    for (var i = 0; i < num2; i++) {
+    for (var i = 0; i < maxNum; i++) {
         result = result + num1; // 2 = 0+2 | 4 = 2+2 | 6= 4+2| i=3 loop out
     }
     return result;
--- q5Multiplication.ts
+++ q5Multiplication.ts
@@ -1,8 +1,11 @@
 function multiplication(num1:number, num2 :number): number{
 
+    const maxNum=num1>num2 ? num1 : num2;
+    const newMax=maxNum!=num1 ? num1 : num2;
+    
     let result = 0;
 
-    for (let i = 0; i<num2; i++){
+    for (let i = 0; i<maxNum; i++){
         result = result + num1; // 2 = 0+2 | 4 = 2+2 | 6= 4+2| i=3 loop out
     }
 return result;
